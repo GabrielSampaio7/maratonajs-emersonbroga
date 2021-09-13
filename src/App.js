2d1e50246e8c4d6c7a3fa8d82b7431edb24004d7
GabrielSampaio7 <gabriel.sampaio@viannasempre.com.br>
2021-09-13 12:35:48 -0300

    aula01 - html, css, react initial

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const App = () => {
+    return (<div className="container">
+        <div className="valid-keys">
+            <span className="matched">Gabri</span>
+            <span className="remainder">el</span>
+        </div>
+        <div className="typed-keys">
+            shuaehusahuxuhasdgabrishuaxhuas
+        </div>
+        <div className="completed-words">
+            <ol>
+                <li>cidade</li>
+                <li>cidade</li>
+                <li>cidade</li>
+                <li>cidade</li>
+                <li>cidade</li>
+            </ol>
+        </div>
+
+    </div>);
+};
+
+export default App;
